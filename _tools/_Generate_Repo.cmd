cd7e33df998025ebb7e101876ad6eba5c57f4759
halikus <optimuscrim@gmail.com>
2015-12-26 18:38:16 -0500

    zip

--- _tools/_Generate_Repo.cmd
+++ _tools/_Generate_Repo.cmd
@@ -4,3 +4,5 @@ cd..
 for /R _repo %%f in (*.zip) do del /q "%%~ff"
 cd _tools
 C:\Python27\python.exe generate_repo.py
+cd..
+echo F | xcopy "_repo\repository.Andy\repository.Andy-0.0.4.zip" "F:\_Github\_AndyRepo\repository.Andy.zip" /cherkyi
